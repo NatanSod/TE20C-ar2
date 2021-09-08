71e08252602a870b78cee510de601a17acab1ea4
Natan Soderstrom <soderstrom.natan@elev.ga.ntig.se>
2021-09-08 11:15:54 +0200

    Jobbat med ConsoleWriteline() och special tecken

---
+++ Programering/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Programering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detta är ett program gjort i C#");
+            Console.WriteLine("Mitt namn är Natan");
+            Console.WriteLine("a\n\"a\"");
+        } 
+    }
+}
---
+++ Uppgift1.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
