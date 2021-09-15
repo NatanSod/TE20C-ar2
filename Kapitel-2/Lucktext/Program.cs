222a928428e0c06fdcb0b3af8b6b250bb636697f
Natan Soderstrom <soderstrom.natan@elev.ga.ntig.se>
2021-09-15 11:11:19 +0200

    Gjorde Lucktext

---
+++ Kapitel-2/Lucktext/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lucktext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("En siffra: ");
+            string år = Console.ReadLine();
+
+            Console.Write("En platts: ");
+            string plats = Console.ReadLine();
+            
+            Console.Write("En person som jobbar med ett yrke: ");
+            string yrke = Console.ReadLine();
+            
+            Console.Write("Ett namn: ");
+            string namn = Console.ReadLine();
+            
+            Console.Write("Ett djur: ");
+            string djur = Console.ReadLine();
+
+            Console.Write("En färg: ");
+            string färg = Console.ReadLine();
+
+            Console.Write("Ett klädesplagg: ");
+            string kläder = Console.ReadLine();
+
+            Console.Write("Ett föremål: ");
+            string föremål = Console.ReadLine();
+            
+
+            Console.WriteLine($"För {år} år sedan, i en stad som hette {plats}, så va en {yrke}, vid namn {namn} på väg hem. Denna {namn} gick i rask takt för att komma hem och ta hand om sin {djur}. Plötsligt blev resan avbruten av en mystisk person, klädd i en {färg} {kläder}. Den mystiska personen gav en {föremål} till {namn}, och sedan försvan. {namn} stoppade det i fickan och fortsatte hem, utan någon aning om vad det skulle leda till.");
+            Console.WriteLine($"Fortsättning följer i nästa avsnit av \"En {yrke} och sin {djur} besegrar Gud\"!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Kapitel-2/Övning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Hej! Vad heter du? ");
+
+            string namn = Console.ReadLine();
+            
+            Console.WriteLine($"Trevligt att träffas, {namn}");
+        }
+    }
+}
