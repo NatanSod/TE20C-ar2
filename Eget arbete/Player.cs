b8dc3960dab011da165180ad83d92a33999012e3
Natan Soderstrom <soderstrom.natan@elev.ga.ntig.se>
2021-09-16 16:30:01 +0200

    Eget arbete: Gå omkring och utforska

--- Eget arbete/Player.cs
+++ Eget arbete/Player.cs
@@ -3,23 +3,46 @@
 namespace Eget_arbete
 {
     public class Player
-    {
-        public static int x = 0, y = 0;
-        public static void Draw () {
-            if (Console.KeyAvailable) {
-                    if (Console.ReadKey().Key == ConsoleKey.RightArrow && x < Program.imgWidth - 1) {
-                        x++;
+    {        
+        public static int x = 9, y = 2;
+        public static void Exist () {
+            switch (Console.ReadKey().Key) {
+                case ConsoleKey.RightArrow:
+                    if (x < Program.imgWidth - 1) {
+                        if (Draw.DungeonAt(x + 1, y)){
+                            x++;
+                        }
                     }
-                    if (Console.ReadKey().Key == ConsoleKey.LeftArrow && x > 0) {
-                        x--;
+                    break;
+                    
+                case ConsoleKey.LeftArrow:
+                    if (x > 0) {
+                        if (Draw.DungeonAt(x - 1, y)){
+                            x--;
+                        }
                     }
-                    if (Console.ReadKey().Key == ConsoleKey.UpArrow && y > 0) {
-                        y--;
+                    break;
+
+                case ConsoleKey.UpArrow:
+                    if (y > 0) {
+                        if (Draw.DungeonAt(x, y - 1)) {
+                            y--;
+                        }
                     }
-                    if (Console.ReadKey().Key == ConsoleKey.DownArrow && y < Program.imgHeight - 1) {
-                        y++;
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    if (y < Program.imgHeight - 1) {
+                        if (Draw.DungeonAt(x, y + 1)) {
+                            y++;
+                        }
                     }
-                }
+                    break;
+
+                case ConsoleKey.Escape:
+                    Program.done = true;
+                    break;
+            }
             Program.image[x,y] = 'ÿ';
         }
     }
--- Eget arbete/Program.cs
+++ Eget arbete/Program.cs
@@ -4,24 +4,45 @@ namespace Eget_arbete
 {
     class Program
     {
-        static bool done = false;
+        static public bool done = false;
 
-        public static int imgWidth = 20, imgHeight = 5;
+        public static int imgWidth = 40, imgHeight = 20;
         public static char[,] image = new char[imgWidth,imgHeight];
         static string finImg;
+        static int wait = 0;
+
 
         static void Main(string[] args)
         {
+            //Denna kod kommer fortsätta att köras tills att "done" är sant.
             while (! done) {
-                ClearCanvas();
-                Player.Draw();
-                PrintCanvas();
+                    if (wait == 0) {
+                    ClearCanvas();
+                    Draw.Dungeon();
+                    Player.Exist();
+                    PrintCanvas();
 
-                if (Console.KeyAvailable) {
+                    //"wait" är till för att begränsa hur ofta en ny bild kan visas, om den visas för ofta så ser saker konstiga ut
+                    //Om du vill se, tillfälligt ändra "wait = 10" till "wait = 1"
+                    wait = 10;
+                } else {
+                    //Om man håller in en rikting under väntetiden så sparas signalen den tills nästa "ReadKey()"
+                    //Det här "äter" dessa signaler.
+                    if (Console.KeyAvailable) {
+                        Console.ReadKey();
+                    } else {
+                        wait = 1;
+                    }
+                }
+                System.Threading.Thread.Sleep(10);
+                wait --;
+
+                //System.Threading.Thread.Sleep(100);
+                /*if (Console.KeyAvailable) {
                     if (Console.ReadKey().Key == ConsoleKey.Escape) {
                         done = true;
                     }
-                }
+                }*/
             }
 
 
@@ -43,6 +64,7 @@ namespace Eget_arbete
                 }
                 finImg = finImg + "\n";
             }
+            Console.WriteLine("");
             Console.WriteLine(finImg);
         }
     }
