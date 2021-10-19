ac84f944b551a6ee891b50760cfe8e87d7f77285
Natan Soderstrom <soderstrom.natan@elev.ga.ntig.se>
2021-10-19 10:32:41 +0200

    För Lööp

---
+++ Kapitel-4/ForLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Console.WriteLine("Räkna 1 till 10!");
+            
+            for (int i = 0; i < 11; i++){
+                Console.WriteLine($"Loop nr {i}");
+            } 
+            */
+
+            //Läs meddelande
+            Console.WriteLine("Skriv ett meddelande");
+            string meddelande = Console.ReadLine();
+            
+            //Skriv en bokstav per rad.
+            for (int i = 0; i < meddelande.Length; i++){
+                Console.WriteLine(meddelande[i]);
+            }
+        }
+    }
+}
